799bd33a9a4a20ed88c88cec4435eb8148e08666
Romelio <joeloporta36@gmail.com>
2026-04-11 15:28:52 -0600

    mejore la chamba de Jeremy

--- 3-Js/de_página_del_mapa.js
+++ 3-Js/de_página_del_mapa.js
@@ -66,3 +66,32 @@ mapa.appendChild(pin);
 
 // INICIO
 mostrar(data);
+
+document.addEventListener("DOMContentLoaded", function(){
+    const botonHamburguesa = document.getElementById("hamburguesa")
+    const menu = document.getElementById("menu")
+    const header = document.querySelector("header");
+    const enlacesMenu = document.querySelectorAll(".menu a")
+
+    if(botonHamburguesa && menu) {
+        botonHamburguesa.addEventListener("click", function(){
+            menu.classList.toggle("activo");
+            botonHamburguesa.classList.toggle("abierto")
+
+        });
+
+        enlacesMenu.forEach(function(enlace){
+            enlace.addEventListener("click", function(){
+                menu.classList.remove("activo")
+                botonHamburguesa.classList.remove("abierto")
+            })
+        })
+    }
+    window.addEventListener("scroll", function(){
+        if (window.scrollY > 50){
+            header.classList.add("scrolled")
+        }else{
+            header.classList.remove("scrolled")
+        }
+    })
+})
